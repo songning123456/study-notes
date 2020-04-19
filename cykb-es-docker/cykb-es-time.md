091d9c9c4a3a579f19ae47464a6b6e0ca3e954c4
songning123456 <1457065857@qq.com>
2020-04-19 16:58:00 +0800

    cykb-es-time

---
+++ cykb-es-docker/cykb-es-time.md
@@ -0,0 +1,16 @@
+cd /home/songning/pro/cykb-es-time
+git clone https://github.com/songning123456/cykb-es-time.git (USE HTTPS)
+cd cykb-es-time/
+mvn clean install -DskipTests
+cp ./target/cykb-es-time-1.0.0-SNAPSHOT.jar ../
+cd ..
+rm -rf cykb-es-time
+vim Dockerfile
+    FROM java:8
+    EXPOSE 8022
+    COPY cykb-es-time-1.0.0-SNAPSHOT.jar /cykb/cykb-es-time.jar
+    CMD ["java", "-jar", "/cykb/cykb-es-time.jar"]
+    RUN echo "Asia/Shanghai" > /etc/timezone
+    ENV LANG C.UTF-8
+docker build -t cykb-es-time_image -f Dockerfile .
+docker run --restart=always --net=host --name cykb-es-time_container -d cykb-es-time_image
---
+++ cykb-es-docker/cykb-es.time.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd /home/songning/pro/cykb-es-time
+rm -rf cykb-es-time-1.0.0-SNAPSHOT.jar
+git clone https://github.com/songning123456/cykb-es-time.git
+cd cykb-es-time/
+mvn clean install -DskipTests
+cp ./target/cykb-es-time-1.0.0-SNAPSHOT.jar ../
+cd ..
+rm -rf cykb-es-time
+containerName=cykb-es-time_container
+exist=`docker inspect --format '{{.State.Running}}' ${containerName}`
+if [ "${exist}" == "true" ]; then
+      docker stop ${containerName}
+fi
+docker rm ${containerName}
+docker rmi cykb-es-time_image
+docker build -t cykb-es-time_image -f Dockerfile .
+docker run --net=host --name cykb-es-time_container -d --restart=always cykb-es-time_image
+
+# 用notepade转为unicode然后上传
+# chmod u+x cykb-es.time.sh
