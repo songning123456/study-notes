a3456704d12bdeb2bc55ed7ae5a0be7df8710a1d
songning123456 <1457065857@qq.com>
2020-04-23 10:46:04 +0800

    cykb-es-content

---
+++ cykb-es-docker/cykb-es-content.md
@@ -0,0 +1,15 @@
+cd /home/songning/pro/cykb-es-content
+git clone https://github.com/songning123456/cykb-es-content.git (USE HTTPS)
+cd cykb-es-content/
+mvn clean install -DskipTests
+cp ./target/content-1.0.0-SNAPSHOT.jar ../
+cd ..
+rm -rf cykb-es-content
+vim Dockerfile
+    FROM java:8
+    COPY content-1.0.0-SNAPSHOT.jar /cykb/cykb-es-content.jar
+    CMD ["java", "-jar", "/cykb/cykb-es-content.jar"]
+    RUN echo "Asia/Shanghai" > /etc/contentzone
+    ENV LANG C.UTF-8
+docker build -t cykb-es-content_image -f Dockerfile .
+docker run --restart=always --net=host --name cykb-es-content_container -d cykb-es-content_image
---
+++ cykb-es-docker/cykb-es-content.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd /home/songning/pro/cykb-es-content
+rm -rf content-1.0.0-SNAPSHOT.jar
+git clone https://github.com/songning123456/cykb-es-content.git
+cd cykb-es-content/
+mvn clean install -DskipTests
+cp ./target/content-1.0.0-SNAPSHOT.jar ../
+cd ..
+rm -rf cykb-es-content
+containerName=cykb-es-content_container
+exist=`docker inspect --format '{{.State.Running}}' ${containerName}`
+if [ "${exist}" == "true" ]; then
+      docker stop ${containerName}
+fi
+docker rm ${containerName}
+docker rmi cykb-es-content_image
+docker build -t cykb-es-content_image -f Dockerfile .
+docker run --net=host --name cykb-es-content_container -d --restart=always cykb-es-content_image
+
+# 用notepade转为unicode然后上传
+# chmod u+x cykb-es-content.sh
