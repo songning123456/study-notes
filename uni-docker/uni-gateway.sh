a1a00930a77922e4d22cf396f5274db959241cfa
songning123456 <1457065857@qq.com>
2020-04-30 22:46:38 +0800

    bug解决

--- uni-docker/uni-gateway.sh
+++ uni-docker/uni-gateway.sh
@@ -16,7 +16,7 @@ fi
 docker rm ${containerName}
 docker rmi uni-gateway_image
 docker build -t uni-gateway_image -f Dockerfile .
-docker run --net=host --name uni-gateway_container -d --restart=always uni-gatewayimage
+docker run --net=host --name uni-gateway_container -d --restart=always uni-gateway_image
 
 # 用notepade转为unicode然后上传
 # chmod u+x uni-gateway.sh
