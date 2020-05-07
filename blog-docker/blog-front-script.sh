c672ac6c2a054784fc7a05b9634d7e1c3a505793
songning <1457065857@qq.com>
2020-05-07 15:47:00 +0800

    修改

--- blog-docker/blog-front-script.sh
+++ blog-docker/blog-front-script.sh
@@ -6,8 +6,8 @@ tar -zxvf dist.tar.gz
 docker stop blog-front_container
 docker rm blog-front_container
 docker rmi blog-front_image
-docker build -t blog-front_image -f dockerfile-blog-front .
-docker run --name blog-front_container -d -p 8070:80 --link blog-server_container blog-front_image
+docker build -t blog-front_image -f Dockerfile .
+docker run --name blog-front_container -d -p 8070:80 --restart=always -v /etc/localtime:/etc/localtime blog-front_image
 
 
 # chmod u+x blog-front-script.sh // 添加文件执行权限
